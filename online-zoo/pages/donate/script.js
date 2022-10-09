c17d0bb8cfed562cd41a16a2179ea67de3c8f024
Joramba <jorkabozko@gmail.com>
2022-10-09 22:58:49 +0200

    feat: add burger menu, 2 carousels and popUp menu for landing page

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector('.header_burger'),
     burgerMenu = document.querySelector('.burger'),
-    burgerClose = document.querySelector('.burger_close');
+    burgerClose = document.querySelector('.burger_close'),
+    background = document.querySelector('.background');
 
 [`load`, `resize`].forEach(it => {
     window.addEventListener(it, () => {
@@ -10,11 +11,18 @@ const burger = document.querySelector('.header_burger'),
     });
 });
 
-
 burger.addEventListener('click', () => {
     burgerMenu.classList.add('open');
+
+    document.body.style.overflow = 'hidden';
+    background.style.display = 'block';
 });
 
-burgerClose.addEventListener('click', () => {
-    burgerMenu.classList.remove('open');
-});
+[burgerClose, background].forEach(item => {
+    item.addEventListener('click', () => {
+        burgerMenu.classList.remove('open');
+
+        document.body.style.overflow = '';
+        background.style.display = 'none';
+    });
+})
