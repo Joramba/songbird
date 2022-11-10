96780e76a0bf5fb1db85249b42e26e13b2973726
Joramba <jorkabozko@gmail.com>
2022-11-10 21:47:02 +0100

    feat: add dynamic creating of answers

--- songbird/pages/quiz/index.js
+++ songbird/pages/quiz/index.js
@@ -1,4 +1,16 @@
-console.log(2121)
-console.log(2121)
-console.log(2121)
-console.log(2121)
+import birdsData from "../../birdsData.js";
+
+const answers = document.querySelector('.answers-group');
+
+function getAnswers(i) {
+    birdsData[i].forEach(item => {
+        answers.innerHTML += `
+        <li class="answers-group-item">
+            <span class="li-btn"></span>
+            ${item.name}
+        </li>
+        `;
+    })
+}
+
+getAnswers(0);
