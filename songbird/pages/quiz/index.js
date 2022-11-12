2bff67f6ea573ceb9051a9c2e7ee937fde5d2d94
Joramba <jorkabozko@gmail.com>
2022-11-12 11:58:28 +0100

    feat: add dynamic content for result page and fix some bugs in quiz page

--- songbird/pages/quiz/index.js
+++ songbird/pages/quiz/index.js
@@ -3,23 +3,20 @@ import birdsData from "../../birdsData.js";
 const answers = document.querySelector('.answers-group'),
     pageItem = document.querySelectorAll('.page-item'),
     instruction = document.querySelector('.instruction'),
+    randomBirdCard = document.querySelector('.random-bird'),
     cardBody = document.querySelector('.card-body'),
     cardDescription = document.querySelector('.card-description'),
-    score = document.querySelector('.score');
+    score = document.querySelector('.score'),
+    nextLvlBtn = document.querySelector('.btn');
 
 let winBird = randomBird(0),
     sum = 0,
     i = 0;
-console.log(winBird);
+
 
 instruction.style.display = 'flex';
 cardBody.style.display = 'none';
 cardDescription.style.display = 'none';
-
-function slides(i) {
-    winBird = randomBird(0);
-
-}
 
 
 function randomBird(i) {
@@ -27,6 +24,65 @@ function randomBird(i) {
 }
 
 let s = 5;
+
+const nextSlide = () => {
+    i++;
+
+    if (i <= 5) {
+        pageItem[i].classList.add('active');
+    } else {
+        localStorage.setItem('score', sum);
+        window.location.href = '../results/index.html';
+    }
+
+
+    createRandomCard();
+    instruction.style.display = 'flex';
+    cardBody.style.display = 'none';
+    cardDescription.style.display = 'none';
+    winBird = randomBird(i);
+    getAnswers(i);
+}
+
+function createRandomCard() {
+    randomBirdCard.innerHTML = `
+    <img src="../../assets/images/random-bird.jpg" alt="Random Bird">
+            <div>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <h3>******</h3>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="audio-player">
+                            <div class="controls">
+                                <div class="play-button">
+                                    <svg viewBox="-200 0 1200 1000">
+                                        <path fill="#00bc8c"
+                                            d="M96.51 11.97c-31.23 8.05-53.26 32.76-63.42 71.27-3.45 12.84-3.64 29.7-3.64 416.71s.19 403.87 3.64 416.71c16.09 60.74 61.69 86.03 120.9 67.25 9-2.87 53.65-25.1 116.49-58.24 56.14-29.51 221.29-116.3 367.28-192.93 145.99-76.64 271.29-143.31 278.38-148.1 39.28-25.68 59.59-63.04 53.26-97.52-4.79-26.63-24.33-53.07-52.88-71.65C892 399.37 172.58 22.32 154.95 16.38c-18.97-6.33-43.3-8.24-58.44-4.41z">
+                                        </path>
+                                    </svg>
+                                </div>
+                                <div class="timebar">
+                                    <div class="timebar-bar">
+
+                                    </div>
+                                    <div class="timebar-circle">
+
+                                    </div>
+                                    <div class="timebar-info">
+                                        <div>00:00</div>
+                                        <div>02:03</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+    `;
+}
+
+createRandomCard();
 
 
 let cardFunction = (e) => {
@@ -87,7 +143,8 @@ let cardFunction = (e) => {
                 answersItems.forEach(elem => {
                     elem.removeEventListener('click', cardFunction)
                 })
-                document.querySelector('.random-bird').innerHTML = `
+
+                randomBirdCard.innerHTML = `
                     <img src=${bird.image} alt="Random Bird">
                     <div>
                         <ul class="list-group">
@@ -123,15 +180,13 @@ let cardFunction = (e) => {
                     </div>
                 `;
 
-                document.querySelector('.btn').classList.add('btn-next');
+                nextLvlBtn.classList.add('btn-next');
 
-                document.querySelector('.btn').addEventListener('click', () => {
-                    getAnswers(++i);
-                })
-
+                nextLvlBtn.addEventListener('click', nextSlide);
             } else {
                 e.target.classList.add('error');
                 s--;
+                e.target.removeEventListener('click', cardFunction);
                 console.log(s);
             }
         }
@@ -146,9 +201,14 @@ function getAnswers(i) {
 
     if (i <= 5) {
         pageItem[i].classList.add('active');
-    } else {
-        window.location.href = '../results/index.html';
     }
+
+    nextLvlBtn.classList.remove('btn-next');
+    nextLvlBtn.removeEventListener('click', nextSlide);
+
+    s = 5;
+    console.log(sum);
+    console.log(winBird);
 
     answers.innerHTML = ``;
     birdsData[i].forEach(item => {
@@ -168,7 +228,6 @@ function getAnswers(i) {
 
 
 getAnswers(i);
-
 
 
 
--- songbird/pages/results/index.js
+++ songbird/pages/results/index.js
@@ -1,3 +1,23 @@
+const box = document.querySelector('.box');
 
-
-
+if (localStorage.getItem('score') == 30) {
+    box.innerHTML = `
+    <div class="box-message">
+        Congrats, you got the maximum score :))
+    </div>
+    <img src="../../assets/images/result-win.png" alt="Meme result win">
+    <a href='../quiz/index.html' class="btn">
+        Try Again
+    </a>
+    `;
+} else {
+    box.innerHTML = `
+    <div class="box-message">
+        You have not scored the maximum number of points ):
+    </div>
+    <img src="../../assets/images/result-lose.png" alt="Meme result lose">
+    <a href='../quiz/index.html' class="btn">
+        Try Again
+    </a>
+    `;
+}
