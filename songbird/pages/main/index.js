887822b1b9febe553272f9ce933bed5916af9c6e
Joramba <jorkabozko@gmail.com>
2022-11-17 12:43:27 +0100

    refactor: rafactor main page

--- songbird/pages/main/index.js
+++ songbird/pages/main/index.js
@@ -1,7 +1,3 @@
-import birdsData from "../../birdsData.js";
-
-console.log(birdsData)
-
 if (localStorage.getItem('max-score') == null) {
     localStorage.setItem('max-score', 0)
 }
--- songbird/pages/quiz/index.js
+++ songbird/pages/quiz/index.js
@@ -1,4 +1,5 @@
-import birdsData from "../../birdsData.js";
+import birdsDataRu from "../../birdsDataRu.js";
+import birdsDataEn from "../../birdDataEn.js"
 
 const answers = document.querySelector('.answers-group'),
     pageItem = document.querySelectorAll('.page-item'),
@@ -27,7 +28,7 @@ winSound.volume = 0.03;
 loseSound.volume = 0.03;
 
 function randomBird(i) {
-    return birdsData[i][Math.floor(Math.random() * (5 - 1 + 1)) + 1];
+    return birdsDataRu[i][Math.floor(Math.random() * (5 - 1 + 1)) + 1];
 }
 
 let s = 5;
@@ -52,11 +53,6 @@ const nextSlide = () => {
 }
 
 function createRandomCard() {
-    // console.log(audio);
-
-    // console.log(Math.floor(audio.duration,1));
-
-
     randomBirdCard.innerHTML = `
     <img src="../../assets/images/random-bird.jpg" alt="Random Bird">
             <div>
@@ -96,6 +92,8 @@ function createRandomCard() {
 
     const audio = document.querySelector('.audio');
     const playBtn = document.querySelector('.play-button');
+    const timebar = document.querySelector('.timebar-bar');
+    const timebarCircle = document.querySelector('.timebar-circle');
 
     playBtn.addEventListener('click', () => {
         if (audio.paused) {
@@ -128,6 +126,22 @@ function createRandomCard() {
         document.querySelector('.time-end').textContent = `${m}:${s}`
     };
 
+    timebar.addEventListener("click", (e) => {
+        const timelineWidth = window.getComputedStyle(timebar).width;
+        const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+        audio.currentTime = timeToSeek;
+        progress = audio.currentTime / audio.duration * 100 + "%";
+        timebarCircle.style.left = progress;
+        timebar.style.background = `linear-gradient(to right, rgb(0, 188, 140) 0%, rgb(61, 133, 140) ${progress}, rgb(115, 115, 115) ${progress}, rgb(115, 115, 115) 100%)`;
+    }, false);
+
+    let progress;
+
+    setInterval(() => {
+        progress = audio.currentTime / audio.duration * 100 + "%";
+        timebarCircle.style.left = progress;
+        timebar.style.background = `linear-gradient(to right, rgb(0, 188, 140) 0%, rgb(61, 133, 140) ${progress}, rgb(115, 115, 115) ${progress}, rgb(115, 115, 115) 100%)`;
+    }, 500);
 }
 
 createRandomCard();
@@ -139,7 +153,7 @@ let cardFunction = (e) => {
     cardBody.style.display = 'flex';
     cardDescription.style.display = 'flex';
 
-    birdsData[i].forEach(bird => {
+    birdsDataRu[i].forEach(bird => {
         if (bird.name == e.target.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim()) {
             cardBody.innerHTML = `
                 <img src=${bird.image} alt="Bird">
@@ -161,11 +175,11 @@ let cardFunction = (e) => {
                                         </path>
                                     </svg>
                                 </div>
-                                <div class="timebar">
+                                <div class="timebar_card">
                                     <div class="card-bar">
 
                                     </div>
-                                    <div class="timebar-circle">
+                                    <div class="card-circle">
 
                                     </div>
                                     <div class="timebar-info">
@@ -180,6 +194,9 @@ let cardFunction = (e) => {
                 `;
             const audio = document.querySelectorAll('.audio')[1];
             const playBtn = document.querySelectorAll('.play-button')[1];
+
+            const timebar = document.querySelector('.card-bar');
+            const timebarCircle = document.querySelector('.card-circle');
             console.log(playBtn);
 
             playBtn.addEventListener('click', () => {
@@ -198,6 +215,23 @@ let cardFunction = (e) => {
                             </svg>`;
                 }
             })
+
+            timebar.addEventListener("click", (e) => {
+                const timelineWidth = window.getComputedStyle(timebar).width;
+                const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+                audio.currentTime = timeToSeek;
+                progress = audio.currentTime / audio.duration * 100 + "%";
+                timebarCircle.style.left = progress;
+                timebar.style.background = `linear-gradient(to right, rgb(0, 188, 140) 0%, rgb(61, 133, 140) ${progress}, rgb(115, 115, 115) ${progress}, rgb(115, 115, 115) 100%)`;
+            }, false);
+
+            let progress;
+
+            setInterval(() => {
+                progress = audio.currentTime / audio.duration * 100 + "%";
+                timebarCircle.style.left = progress;
+                timebar.style.background = `linear-gradient(to right, rgb(0, 188, 140) 0%, rgb(61, 133, 140) ${progress}, rgb(115, 115, 115) ${progress}, rgb(115, 115, 115) 100%)`;
+            }, 500);
 
             audio.onloadedmetadata = function () {
                 let m = Math.floor(audio.duration / 60, 1);
@@ -268,40 +302,57 @@ let cardFunction = (e) => {
 
                 const audio = document.querySelectorAll('.audio')[0];
                 const playBtn = document.querySelectorAll('.play-button')[0];
-                console.log(playBtn);
-    
+                const timebar = document.querySelector('.timebar-bar');
+                const timebarCircle = document.querySelector('.timebar-circle');
+            
                 playBtn.addEventListener('click', () => {
                     if (audio.paused) {
                         audio.play();
                         playBtn.innerHTML = `
-                                <svg viewBox="0 0 47.607 47.607"><path fill="#00bc8c" d="M17.991 40.976a6.631 6.631 0 01-13.262 0V6.631a6.631 6.631 0 0113.262 0v34.345zM42.877 40.976a6.631 6.631 0 01-13.262 0V6.631a6.631 6.631 0 0113.262 0v34.345z"></path></svg>
-                                `;
+                        <svg viewBox="0 0 47.607 47.607"><path fill="#00bc8c" d="M17.991 40.976a6.631 6.631 0 01-13.262 0V6.631a6.631 6.631 0 0113.262 0v34.345zM42.877 40.976a6.631 6.631 0 01-13.262 0V6.631a6.631 6.631 0 0113.262 0v34.345z"></path></svg>
+                        `;
                     } else {
                         audio.pause();
                         playBtn.innerHTML = `                                    
-                                <svg viewBox="-200 0 1200 1000">
-                                    <path fill="#00bc8c"
-                                        d="M96.51 11.97c-31.23 8.05-53.26 32.76-63.42 71.27-3.45 12.84-3.64 29.7-3.64 416.71s.19 403.87 3.64 416.71c16.09 60.74 61.69 86.03 120.9 67.25 9-2.87 53.65-25.1 116.49-58.24 56.14-29.51 221.29-116.3 367.28-192.93 145.99-76.64 271.29-143.31 278.38-148.1 39.28-25.68 59.59-63.04 53.26-97.52-4.79-26.63-24.33-53.07-52.88-71.65C892 399.37 172.58 22.32 154.95 16.38c-18.97-6.33-43.3-8.24-58.44-4.41z">
-                                    </path>
-                                </svg>`;
+                        <svg viewBox="-200 0 1200 1000">
+                            <path fill="#00bc8c"
+                                d="M96.51 11.97c-31.23 8.05-53.26 32.76-63.42 71.27-3.45 12.84-3.64 29.7-3.64 416.71s.19 403.87 3.64 416.71c16.09 60.74 61.69 86.03 120.9 67.25 9-2.87 53.65-25.1 116.49-58.24 56.14-29.51 221.29-116.3 367.28-192.93 145.99-76.64 271.29-143.31 278.38-148.1 39.28-25.68 59.59-63.04 53.26-97.52-4.79-26.63-24.33-53.07-52.88-71.65C892 399.37 172.58 22.32 154.95 16.38c-18.97-6.33-43.3-8.24-58.44-4.41z">
+                            </path>
+                        </svg>`;
                     }
                 })
-    
+            
                 audio.onloadedmetadata = function () {
                     let m = Math.floor(audio.duration / 60, 1);
                     if (m < 10) {
                         m = `0${m}`
                     }
-    
+            
                     let s = Math.round(audio.duration % 60);
                     if (s < 10) {
                         s = `0${s}`
                     }
-                    document.querySelectorAll('.time-end')[0].textContent = `${m}:${s}`
-                    document.querySelectorAll('.time-end')[1].textContent = `${m}:${s}`
+            
+                    document.querySelector('.time-end').textContent = `${m}:${s}`
                 };
-
-
+            
+                timebar.addEventListener("click", (e) => {
+                    const timelineWidth = window.getComputedStyle(timebar).width;
+                    const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+                    audio.currentTime = timeToSeek;
+                    progress = audio.currentTime / audio.duration * 100 + "%";
+                    timebarCircle.style.left = progress;
+                    timebar.style.background = `linear-gradient(to right, rgb(0, 188, 140) 0%, rgb(61, 133, 140) ${progress}, rgb(115, 115, 115) ${progress}, rgb(115, 115, 115) 100%)`;
+                }, false);
+            
+                let progress;
+            
+                setInterval(() => {
+                    progress = audio.currentTime / audio.duration * 100 + "%";
+                    timebarCircle.style.left = progress;
+                    timebar.style.background = `linear-gradient(to right, rgb(0, 188, 140) 0%, rgb(61, 133, 140) ${progress}, rgb(115, 115, 115) ${progress}, rgb(115, 115, 115) 100%)`;
+                }, 500);
+                
                 nextLvlBtn.classList.add('btn-next');
 
                 nextLvlBtn.addEventListener('click', nextSlide);
@@ -337,7 +388,7 @@ function getAnswers(i) {
     console.log(winBird);
 
     answers.innerHTML = ``;
-    birdsData[i].forEach(item => {
+    birdsDataRu[i].forEach(item => {
         answers.innerHTML += `
         <li class="answers-group-item">
             <span class="li-btn"></span>
